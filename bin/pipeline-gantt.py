70291ba748bb77e7422b3cdd52fc874bea9b9d4a
Robert Syme <rob.syme@gmail.com>
2023-11-07 13:03:41 -0500

    Update pipeline-gantt.py
    Use input directory rather than tar.gz file
    
    Remove unused tar module
    
    Change call to python script to reflect new dir (vs file) args
    
    Signed-off-by: Rob Syme <rob.syme@gmail.com>

--- bin/pipeline-gantt.py
+++ bin/pipeline-gantt.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env python3
 
 import json
-import tarfile
 from datetime import datetime
 from typing import IO
+from pathlib import Path
 
 import click
 import pandas as pd
 import plotly.express as px
 
 
-def extract_instance(fusion_logs: str, lines: IO) -> str:
-    for i, line in enumerate(lines):
-        try:
-            log = json.loads(line)
-            if 'instance-id' in log:
-                return log['instance-id']
-        except json.JSONDecodeError:
-            print(f"WARN: invalid JSON at '{fusion_logs}' line {i}")
-    return ""
-
+def extract_instance(fusion_logs: Path) -> str:
+    with fusion_logs.open() as file:
+        for line_number, line in enumerate(file, start=1):
+            try:
+                log = json.loads(line)
+                if 'instance-id' in log:
+                    return log['instance-id']
+            except json.JSONDecodeError:
+                print(f"WARN: invalid JSON at '{fusion_logs}' line {line_number}")
+        return ""
 
 @click.command()
 @click.option('--title', default='Pipeline GANTT', help='Plot title.')
-@click.option('--input-file', type=click.Path(), help='The pipeline dump tar.gz input file.')
+@click.option('--input-dir', type=click.Path(), help='The pipeline dump tar.gz input file.')
 @click.option('--output-file', type=click.Path(), help='The HTML output file')
-def build_gantt(title: str, input_file: str, output_file: str):
+def build_gantt(title: str, input_dir: str, output_file: str):
     tasks = []
     instance_ids = {}
 
-    tar = tarfile.open(input_file, "r:gz")
-    for member in tar.getmembers():
-        if member.name == "workflow-tasks.json":
-            tasks = json.load(tar.extractfile(member))
-        if member.name.endswith(".fusion.log"):
-            _, task_id, _ = member.name.split('/')
-            instance_id = extract_instance(member.name, tar.extractfile(member))
-            instance_ids[int(task_id)] = instance_id
+    for path in Path(input_dir).glob('workflow-tasks.json'):
+        with path.open() as json_file:
+            tasks = json.load(json_file)
+    for path in Path(input_dir).glob('**/.fusion.log'):
+        task_id = int(path.parent.name)
+        instance_id = extract_instance(path)
+        instance_ids[task_id] = instance_id
 
     for t in tasks:
         t['instanceId'] = instance_ids.get(t['taskId'], "unknow")
