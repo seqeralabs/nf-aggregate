3a96b00f125d7c911c0f7b5543f98256e660d057
Edmund Miller <edmund.miller@seqera.io>
2025-01-07 12:30:50 -0600

    Give up and resort to copy pasting

--- tests/data/export_test.R
+++ tests/data/export_test.R
@@ -1,5 +1,9 @@
 # install.packages("devtools")
 # devtools::install_github("flowuenne/SeqeraBenchmarkR", auth_token = "<your-token>")
-seqeraruncollection = readRDS("test_SeqeraRunCollection_object.rds")
 
-write.csv("")
+seqeraruncollection <- readRDS("tests/data/test_SeqeraRunCollection_object.rds")
+
+output_path <- "tests/data/"
+csv_path = paste0(output_path, "service_info",".csv")
+data = as.matrix(seqeraruncollection@service_info)
+write.csv(csv_path, )
