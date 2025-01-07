0117b7cb969b9cdf575f2807100dc08b67de101d
Edmund Miller <edmund.miller@seqera.io>
2025-01-07 12:08:45 -0600

    chore: Add example test output

---
+++ tests/data/export_test.R
@@ -0,0 +1,5 @@
+# install.packages("devtools")
+# devtools::install_github("flowuenne/SeqeraBenchmarkR", auth_token = "<your-token>")
+seqeraruncollection = readRDS("test_SeqeraRunCollection_object.rds")
+
+write.csv("")
