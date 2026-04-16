seqeralabs/nf-aggregate

commit ca70336767e2be4df22b93e95eb2451cbb1df27c

chore(ci): drop nf-core template workflows

.github/workflows/linting.yml
@@ -1,83 +1,0 @@
-name: nf-core linting
-# This workflow is triggered on pushes and PRs to the repository.
-# It runs the `nf-core pipelines lint` and markdown lint tests to ensure
-# that the code meets the nf-core guidelines.
-on:
-  push:
-    branches:
-      - dev
-  pull_request:
-  release:
-    types: [published]
-
-jobs:
-  pre-commit:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@11bd71901bbe5b1630ceea73d27597364c9af683 # v4
-
-      - name: Set up Python 3.12
-        uses: actions/setup-python@0b93645e9fea7318ecaed2b359559ac225c90a2b # v5
-        with:
-          python-version: "3.12"
-
-      - name: Install pre-commit
-        run: pip install pre-commit
-
-      - name: Run pre-commit
-        run: pre-commit run --all-files
-
-  nf-core:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Check out pipeline code
-        uses: actions/checkout@11bd71901bbe5b1630ceea73d27597364c9af683 # v4
-
-      - name: Install Nextflow
-        uses: nf-core/setup-nextflow@v2
-
-      - uses: actions/setup-python@0b93645e9fea7318ecaed2b359559ac225c90a2b # v5
-        with:
-          python-version: "3.12"
-          architecture: "x64"
-
-      - name: read .nf-core.yml
-        uses: pietrobolcato/action-read-yaml@1.1.0
-        id: read_yml
-        with:
-          config: ${{ github.workspace }}/.nf-core.yml
-
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip
-          pip install nf-core==${{ steps.read_yml.outputs['nf_core_version'] }}
-
-      - name: Run nf-core pipelines lint
-        if: ${{ github.base_ref != 'master' }}
-        env:
-          GITHUB_COMMENTS_URL: ${{ github.event.pull_request.comments_url }}
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-          GITHUB_PR_COMMIT: ${{ github.event.pull_request.head.sha }}
-        run: nf-core -l lint_log.txt pipelines lint --dir ${GITHUB_WORKSPACE} --markdown lint_results.md
-
-      - name: Run nf-core pipelines lint --release
-        if: ${{ github.base_ref == 'master' }}
-        env:
-          GITHUB_COMMENTS_URL: ${{ github.event.pull_request.comments_url }}
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-          GITHUB_PR_COMMIT: ${{ github.event.pull_request.head.sha }}
-        run: nf-core -l lint_log.txt pipelines lint --release --dir ${GITHUB_WORKSPACE} --markdown lint_results.md
-
-      - name: Save PR number
-        if: ${{ always() }}
-        run: echo ${{ github.event.pull_request.number }} > PR_number.txt
-
-      - name: Upload linting log file artifact
-        if: ${{ always() }}
-        uses: actions/upload-artifact@b4b15b8c7c6ac21ea08fcf65892d2ee8f75cf882 # v4
-        with:
-          name: linting-logs
-          path: |
-            lint_log.txt
-            lint_results.md
-            PR_number.txt

.github/workflows/branch.yml
@@ -1,46 +1,0 @@
-name: nf-core branch protection
-# This workflow is triggered on PRs to `main`/`master` branch on the repository
-# It fails when someone tries to make a PR against the nf-core `main`/`master` branch instead of `dev`
-on:
-  pull_request_target:
-    branches:
-      - main
-      - master
-
-jobs:
-  test:
-    runs-on: ubuntu-latest
-    steps:
-      # PRs to the nf-core repo main/master branch are only ok if coming from the nf-core repo `dev` or any `patch` branches
-      - name: Check PRs
-        if: github.repository == 'seqeralabs/nf-aggregate'
-        run: |
-          { [[ ${{github.event.pull_request.head.repo.full_name }} == seqeralabs/nf-aggregate ]] && [[ $GITHUB_HEAD_REF == "dev" ]]; } || [[ $GITHUB_HEAD_REF == "patch" ]]
-
-      # If the above check failed, post a comment on the PR explaining the failure
-      # NOTE - this doesn't currently work if the PR is coming from a fork, due to limitations in GitHub actions secrets
-      - name: Post PR comment
-        if: failure()
-        uses: mshick/add-pr-comment@b8f338c590a895d50bcbfa6c5859251edc8952fc # v2
-        with:
-          message: |
-            ## This PR is against the `${{github.event.pull_request.base.ref}}` branch :x:
-
-            * Do not close this PR
-            * Click _Edit_ and change the `base` to `dev`
-            * This CI test will remain failed until you push a new commit
-
-            ---
-
-            Hi @${{ github.event.pull_request.user.login }},
-
-            It looks like this pull-request is has been made against the [${{github.event.pull_request.head.repo.full_name }}](https://github.com/${{github.event.pull_request.head.repo.full_name }}) ${{github.event.pull_request.base.ref}} branch.
-            The ${{github.event.pull_request.base.ref}} branch on nf-core repositories should always contain code from the latest release.
-            Because of this, PRs to ${{github.event.pull_request.base.ref}} are only allowed if they come from the [${{github.event.pull_request.head.repo.full_name }}](https://github.com/${{github.event.pull_request.head.repo.full_name }}) `dev` branch.
-
-            You do not need to close this PR, you can change the target branch to `dev` by clicking the _"Edit"_ button at the top of this page.
-            Note that even after this, the test will continue to show as failing until you push a new commit.
-
-            Thanks again for your contribution!
-          repo-token: ${{ secrets.GITHUB_TOKEN }}
-          allow-repeats: false

.github/workflows/download_pipeline.yml
@@ -1,91 +1,0 @@
-name: Test successful pipeline download with 'nf-core pipelines download'
-
-# Run the workflow when:
-#  - dispatched manually
-#  - when a PR is opened or reopened to main/master branch
-#  - the head branch of the pull request is updated, i.e. if fixes for a release are pushed last minute to dev.
-on:
-  workflow_dispatch:
-    inputs:
-      testbranch:
-        description: "The specific branch you wish to utilize for the test execution of nf-core pipelines download."
-        required: true
-        default: "dev"
-  pull_request:
-    types:
-      - opened
-      - edited
-      - synchronize
-    branches:
-      - main
-      - master
-  pull_request_target:
-    branches:
-      - main
-      - master
-
-env:
-  NXF_ANSI_LOG: false
-
-jobs:
-  configure:
-    runs-on: ubuntu-latest
-    outputs:
-      REPO_LOWERCASE: ${{ steps.get_repo_properties.outputs.REPO_LOWERCASE }}
-      REPOTITLE_LOWERCASE: ${{ steps.get_repo_properties.outputs.REPOTITLE_LOWERCASE }}
-      REPO_BRANCH: ${{ steps.get_repo_properties.outputs.REPO_BRANCH }}
-    steps:
-      - name: Get the repository name and current branch
-        id: get_repo_properties
-        run: |
-          echo "REPO_LOWERCASE=${GITHUB_REPOSITORY,,}" >> "$GITHUB_OUTPUT"
-          echo "REPOTITLE_LOWERCASE=$(basename ${GITHUB_REPOSITORY,,})" >> "$GITHUB_OUTPUT"
-          echo "REPO_BRANCH=${{ github.event.inputs.testbranch || 'dev' }}" >> "$GITHUB_OUTPUT"
-
-  download:
-    runs-on: ubuntu-latest
-    needs: configure
-    steps:
-      - name: Install Nextflow
-        uses: nf-core/setup-nextflow@v2
-
-      - name: Disk space cleanup
-        uses: jlumbroso/free-disk-space@54081f138730dfa15788a46383842cd2f914a1be # v1.3.1
-
-      - uses: actions/setup-python@0b93645e9fea7318ecaed2b359559ac225c90a2b # v5
-        with:
-          python-version: "3.12"
-          architecture: "x64"
-
-      - name: Setup Apptainer
-        uses: eWaterCycle/setup-apptainer@4bb22c52d4f63406c49e94c804632975787312b3 # v2.0.0
-        with:
-          apptainer-version: 1.3.4
-
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip
-          pip install git+https://github.com/nf-core/tools.git@dev
-
-      - name: Make a cache directory for the container images
-        run: |
-          mkdir -p ./singularity_container_images
-
-      - name: Download the pipeline
-        env:
-          NXF_SINGULARITY_CACHEDIR: ./singularity_container_images
-        run: |
-          nf-core pipelines download ${{ needs.configure.outputs.REPO_LOWERCASE }} \
-          --revision ${{ needs.configure.outputs.REPO_BRANCH }} \
-          --outdir ./${{ needs.configure.outputs.REPOTITLE_LOWERCASE }} \
-          --compress "none" \
-          --container-system 'singularity' \
-          --container-library "quay.io" -l "docker.io" -l "community.wave.seqera.io/library/" \
-          --container-cache-utilisation 'amend' \
-          --download-configuration 'yes'
-
-      - name: Inspect download
-        run: tree ./${{ needs.configure.outputs.REPOTITLE_LOWERCASE }}
-
-      - name: Inspect container images
-        run: tree ./singularity_container_images | tee ./container_initial

.github/workflows/fix-linting.yml
@@ -1,89 +1,0 @@
-name: Fix linting from a comment
-on:
-  issue_comment:
-    types: [created]
-
-jobs:
-  fix-linting:
-    # Only run if comment is on a PR with the main repo, and if it contains the magic keywords
-    if: >
-      contains(github.event.comment.html_url, '/pull/') &&
-      contains(github.event.comment.body, '@nf-core-bot fix linting') &&
-      github.repository == 'seqeralabs/nf-aggregate'
-    runs-on: ubuntu-latest
-    steps:
-      # Use the @nf-core-bot token to check out so we can push later
-      - uses: actions/checkout@11bd71901bbe5b1630ceea73d27597364c9af683 # v4
-        with:
-          token: ${{ secrets.nf_core_bot_auth_token }}
-
-      # indication that the linting is being fixed
-      - name: React on comment
-        uses: peter-evans/create-or-update-comment@71345be0265236311c031f5c7866368bd1eff043 # v4
-        with:
-          comment-id: ${{ github.event.comment.id }}
-          reactions: eyes
-
-      # Action runs on the issue comment, so we don't get the PR by default
-      # Use the gh cli to check out the PR
-      - name: Checkout Pull Request
-        run: gh pr checkout ${{ github.event.issue.number }}
-        env:
-          GITHUB_TOKEN: ${{ secrets.nf_core_bot_auth_token }}
-
-      # Install and run pre-commit
-      - uses: actions/setup-python@0b93645e9fea7318ecaed2b359559ac225c90a2b # v5
-        with:
-          python-version: "3.12"
-
-      - name: Install pre-commit
-        run: pip install pre-commit
-
-      - name: Run pre-commit
-        id: pre-commit
-        run: pre-commit run --all-files
-        continue-on-error: true
-
-      # indication that the linting has finished
-      - name: react if linting finished succesfully
-        if: steps.pre-commit.outcome == 'success'
-        uses: peter-evans/create-or-update-comment@71345be0265236311c031f5c7866368bd1eff043 # v4
-        with:
-          comment-id: ${{ github.event.comment.id }}
-          reactions: "+1"
-
-      - name: Commit & push changes
-        id: commit-and-push
-        if: steps.pre-commit.outcome == 'failure'
-        run: |
-          git config user.email "core@nf-co.re"
-          git config user.name "nf-core-bot"
-          git config push.default upstream
-          git add .
-          git status
-          git commit -m "[automated] Fix code linting"
-          git push
-
-      - name: react if linting errors were fixed
-        id: react-if-fixed
-        if: steps.commit-and-push.outcome == 'success'
-        uses: peter-evans/create-or-update-comment@71345be0265236311c031f5c7866368bd1eff043 # v4
-        with:
-          comment-id: ${{ github.event.comment.id }}
-          reactions: hooray
-
-      - name: react if linting errors were not fixed
-        if: steps.commit-and-push.outcome == 'failure'
-        uses: peter-evans/create-or-update-comment@71345be0265236311c031f5c7866368bd1eff043 # v4
-        with:
-          comment-id: ${{ github.event.comment.id }}
-          reactions: confused
-
-      - name: react if linting errors were not fixed
-        if: steps.commit-and-push.outcome  == 'failure'
-        uses: peter-evans/create-or-update-comment@71345be0265236311c031f5c7866368bd1eff043 # v4
-        with:
-          issue-number: ${{ github.event.issue.number }}
-          body: |
-            @${{ github.actor }} I tried to fix the linting errors, but it didn't work. Please fix them manually.
-            See [CI log](https://github.com/seqeralabs/nf-aggregate/actions/runs/${{ github.run_id }}) for more details.

.github/workflows/linting_comment.yml
@@ -1,28 +1,0 @@
-name: nf-core linting comment
-# This workflow is triggered after the linting action is complete
-# It posts an automated comment to the PR, even if the PR is coming from a fork
-
-on:
-  workflow_run:
-    workflows: ["nf-core linting"]
-
-jobs:
-  test:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Download lint results
-        uses: dawidd6/action-download-artifact@20319c5641d495c8a52e688b7dc5fada6c3a9fbc # v8
-        with:
-          workflow: linting.yml
-          workflow_conclusion: completed
-
-      - name: Get PR number
-        id: pr_number
-        run: echo "pr_number=$(cat linting-logs/PR_number.txt)" >> $GITHUB_OUTPUT
-
-      - name: Post PR comment
-        uses: marocchino/sticky-pull-request-comment@331f8f5b4215f0445d3c07b4967662a32a2d3e31 # v2
-        with:
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-          number: ${{ steps.pr_number.outputs.pr_number }}
-          path: linting-logs/lint_results.md

.github/workflows/nf-test.yml
@@ -84,7 +84,7 @@ jobs:
           - isMain: false
             profile: "singularity"
         NXF_VER:
-          - "24.04.2"
+          - "25.10.0"
           - "latest-everything"
     env:
       NXF_ANSI_LOG: false

.github/workflows/template_version_comment.yml
@@ -1,46 +1,0 @@
-name: nf-core template version comment
-# This workflow is triggered on PRs to check if the pipeline template version matches the latest nf-core version.
-# It posts a comment to the PR, even if it comes from a fork.
-
-on: pull_request_target
-
-jobs:
-  template_version:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Check out pipeline code
-        uses: actions/checkout@11bd71901bbe5b1630ceea73d27597364c9af683 # v4
-        with:
-          ref: ${{ github.event.pull_request.head.sha }}
-
-      - name: Read template version from .nf-core.yml
-        uses: nichmor/minimal-read-yaml@v0.0.2
-        id: read_yml
-        with:
-          config: ${{ github.workspace }}/.nf-core.yml
-
-      - name: Install nf-core
-        run: |
-          python -m pip install --upgrade pip
-          pip install nf-core==${{ steps.read_yml.outputs['nf_core_version'] }}
-
-      - name: Check nf-core outdated
-        id: nf_core_outdated
-        run: echo "OUTPUT=$(pip list --outdated | grep nf-core)" >> ${GITHUB_ENV}
-
-      - name: Post nf-core template version comment
-        uses: mshick/add-pr-comment@b8f338c590a895d50bcbfa6c5859251edc8952fc # v2
-        if: |
-          contains(env.OUTPUT, 'nf-core')
-        with:
-          repo-token: ${{ secrets.NF_CORE_BOT_AUTH_TOKEN }}
-          allow-repeats: false
-          message: |
-            > [!WARNING]
-            > Newer version of the nf-core template is available.
-            >
-            > Your pipeline is using an old version of the nf-core template: ${{ steps.read_yml.outputs['nf_core_version'] }}.
-            > Please update your pipeline to the latest version.
-            >
-            > For more documentation on how to update your pipeline, please see the [nf-core documentation](https://github.com/nf-core/tools?tab=readme-ov-file#sync-a-pipeline-with-the-template) and [Synchronisation documentation](https://nf-co.re/docs/contributing/sync).
-          #

.nf-core.yml
@@ -1,63 +1,0 @@
-lint:
-  base_config: false
-  files_exist:
-    - .github/.dockstore.yml
-    - .github/ISSUE_TEMPLATE/config.yml
-    - .github/workflows/awsfulltest.yml
-    - .github/workflows/awstest.yml
-    - .github/workflows/branch.yml
-    - .github/workflows/linting_comment.yml
-    - CODE_OF_CONDUCT.md
-    - LICENSE
-    - assets/email_template.html
-    - assets/email_template.txt
-    - assets/nf-core-nf-aggregate_logo_light.png
-    - assets/sendmail_template.txt
-    - conf/base.config
-    - conf/igenomes.config
-    - conf/modules.config
-    - conf/test.config
-    - conf/test_full.config
-    - docs/README.md
-    - docs/images/nf-core-nf-aggregate_logo_dark.png
-    - docs/images/nf-core-nf-aggregate_logo_light.png
-    - docs/output.md
-    - docs/usage.md
-  files_unchanged:
-    - .github/ISSUE_TEMPLATE/bug_report.yml
-    - .github/workflows/linting.yml
-    - CODE_OF_CONDUCT.md
-    - assets/nf-aggregate_logo_light.png
-    - docs/images/nf-aggregate_logo_dark.png
-    - docs/images/nf-aggregate_logo_light.png
-  modules_config: false
-  nextflow_config:
-    - custom_config
-    - manifest.homePage
-    - manifest.name
-    - process.cpus
-    - process.memory
-    - process.time
-  pipeline_name_conventions: false
-  schema_lint: false
-  template_strings:
-    - bin/benchmark_report_template.html
-    - assets/AGENTS.md
-  nf_test_content:
-    - tests/nextflow.config
-nf_core_version: 3.3.1
-repository_type: pipeline
-template:
-  author: SciDev Team
-  description: Pipeline to aggregate pertinent metrics across pipeline runs on the
-    Seqera Platform.
-  force: false
-  is_nfcore: false
-  name: nf-aggregate
-  org: seqeralabs
-  outdir: .
-  skip_features:
-    - ci
-    - igenomes
-    - nf_core_configs
-  version: 1.0.0

commit e484e8ad1cefd408303a355e55bab48c14df95e4

fix(ci): drop nf-core lint job without .nf-core.yml; document private CA on Platform
- Remove the nf-core pipelines lint step from linting.yml: dev dropped .nf-core.yml
  in ca70336, which broke the workflow (empty nf-core install and missing config).
- Rename workflow to Pipeline linting; keep action pins aligned with Renovate targets.
- Add README guidance for private CA in task containers using current process names.
- Fix editorconfig continuation indent in NF_AGGREGATE workflow warning.

Co-authored-by: Florian Wuennemann <flowuenne@gmail.com>

.github/workflows/linting.yml
@@ -1,7 +1,5 @@
-name: nf-core linting
-# This workflow is triggered on pushes and PRs to the repository.
-# It runs the `nf-core pipelines lint` and markdown lint tests to ensure
-# that the code meets the nf-core guidelines.
+name: Pipeline linting
+# Runs pre-commit (Prettier, editorconfig, etc.) on pushes to dev and on pull requests.
 on:
   push:
     branches:
@@ -26,58 +24,3 @@ jobs:
 
       - name: Run pre-commit
         run: pre-commit run --all-files
-
-  nf-core:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Check out pipeline code
-        uses: actions/checkout@34e114876b0b11c390a56381ad16ebd13914f8d5 # v4.3.1
-
-      - name: Install Nextflow
-        uses: nf-core/setup-nextflow@v2
-
-      - uses: actions/setup-python@a26af69be951a213d495a4c3e4e4022e16d87065 # v5.6.0
-        with:
-          python-version: "3.12"
-          architecture: "x64"
-
-      - name: read .nf-core.yml
-        uses: pietrobolcato/action-read-yaml@1.1.0
-        id: read_yml
-        with:
-          config: ${{ github.workspace }}/.nf-core.yml
-
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip
-          pip install nf-core==${{ steps.read_yml.outputs['nf_core_version'] }}
-
-      - name: Run nf-core pipelines lint
-        if: ${{ github.base_ref != 'master' }}
-        env:
-          GITHUB_COMMENTS_URL: ${{ github.event.pull_request.comments_url }}
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-          GITHUB_PR_COMMIT: ${{ github.event.pull_request.head.sha }}
-        run: nf-core -l lint_log.txt pipelines lint --dir ${GITHUB_WORKSPACE} --markdown lint_results.md
-
-      - name: Run nf-core pipelines lint --release
-        if: ${{ github.base_ref == 'master' }}
-        env:
-          GITHUB_COMMENTS_URL: ${{ github.event.pull_request.comments_url }}
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-          GITHUB_PR_COMMIT: ${{ github.event.pull_request.head.sha }}
-        run: nf-core -l lint_log.txt pipelines lint --release --dir ${GITHUB_WORKSPACE} --markdown lint_results.md
-
-      - name: Save PR number
-        if: ${{ always() }}
-        run: echo ${{ github.event.pull_request.number }} > PR_number.txt
-
-      - name: Upload linting log file artifact
-        if: ${{ always() }}
-        uses: actions/upload-artifact@65462800fd760344b1a7b4382951275a0abb4808 # v4
-        with:
-          name: linting-logs
-          path: |
-            lint_log.txt
-            lint_results.md
-            PR_number.txt

workflows/nf_aggregate/main.nf
@@ -33,7 +33,7 @@ workflow NF_AGGREGATE {
         ch_split.api.count().subscribe { n ->
             if (n > 0) {
                 log.warn "Found ${n} API run(s) but --generate_benchmark_report is not enabled. " +
-                         "API runs will not produce any output. Enable --generate_benchmark_report to process them."
+                    "API runs will not produce any output. Enable --generate_benchmark_report to process them."
             }
         }
     }